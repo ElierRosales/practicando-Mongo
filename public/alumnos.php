b328afb19d352899322a1d3522c6a3a955777a20
Juan Elier Rosales Rosas <81974347+ElierRosales@users.noreply.github.com>
2024-09-14 17:52:41 -0600

    Creando tabla para mostrar los registros
    Comenzando con la maquetación de la página para después conectar la base de datos creada con Mongodb

---
+++ public/alumnos.php
@@ -0,0 +1,16 @@
+<?php include "templates/encabezado.php"?>
+
+<!--Solo para empezar, esto mandarlo a la página del formulario cuando quede lista la base de datos -->
+<div class="card mt-4">
+  <div class="card-body">
+    <div class="container">
+        <div class="row justify-content-center align-items-center">
+            <div class="col">Alumno</div>
+            <div class="col">Materia</div>
+            <div class="col">Calificación</div>
+        </div>
+    </div>
+  </div>
+  <a name="returnAlumnos" id="returnAlumnos" class="btn btn-primary" href="index.php" role="button">Regresar</a>
+  
+</div>
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php include "templates/encabezado.php"?>
+<h1>CRUD con PHP y está vez utilizando MongoDB</h1>
+<a name="registroAlumnos" id="botonRegistro" class="btn btn-primary" href="alumnos.php" role="button">Registro de alumnos</a>
+
+
+
+<?php include "templates/fotter.php"?>
