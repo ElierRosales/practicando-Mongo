f6a7836bb097979b435c4852590a43d4228513de
ElierRosales <2172040721@correo.ler.uam.mx>
2024-09-17 15:03:15 -0600

    probando configuración con mongo

--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
+<?php
+echo phpinfo();
+?>
 <?php include "templates/encabezado.php"?>
 <h1>CRUD con PHP y está vez utilizando MongoDB</h1>
 <a name="registroAlumnos" id="botonRegistro" class="btn btn-primary" href="alumnos.php" role="button">Registro de alumnos</a>
